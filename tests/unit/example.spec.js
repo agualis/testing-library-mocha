fefacfa6f255e37450cab30665b4a76d6eac84e1
Paqui <paqui.calabria@schibsted.com>
2020-01-14 14:50:23 +0100

    Add testing-library

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,32 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import {render, fireEvent} from '@testing-library/vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
+import App from '../../src/App.vue'
+import Home from '../../src/views/Home.vue'
+import About from '../../src/views/About.vue'
+
+const routes = [
+  {path: '/', component: Home},
+  {path: '/about', component: About},
+  {path: '*', redirect: '/about'},
+]
+it('full app rendering/navigating', async () => {
+  // Notice how we pass a `routes` object to our render function.
+  const {queryByTestId} = render(App, {routes})
+
+  expect(queryByTestId('location-display').textContent).toEqual('/')
+
+  await fireEvent.click(queryByTestId('about-link'))
+
+  expect(queryByTestId('location-display').textContent).toEqual('/about')
+})
+
+it('setting initial route', () => {
+  // The callback function receives three parameters: the Vue instance where
+  // the component is mounted, the store instance (if any) and the router
+  // object.
+  const {queryByTestId} = render(App, {routes}, (vue, store, router) => {
+    router.push('/about')
   })
+
+  expect(queryByTestId('location-display').textContent).toEqual('/about')
 })
